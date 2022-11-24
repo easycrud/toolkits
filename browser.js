b14440a85de2a1cc1eef0097b61adca5fbe3e688
xilesun <2013xile@gmail.com>
2022-11-24 17:08:39 +0800

    support def2jsonschema

--- browser.js
+++ browser.js
@@ -1,3 +1,5 @@
 module.exports = {
   parseContent: require('./lib/parser/util'),
+  def2jsonschema: require('./lib/converter/json.schema').def2jsonschema,
+  def2formily: require('./lib/converter/json.schema').def2formily,
 };
